f82d5898c843d5d2fd28f0a5179f71dd7d801a6a
MrunaliSawant13 <113444243+MrunaliSawant13@users.noreply.github.com>
2023-04-19 14:27:52 +0530

    Assignment3

---
+++ Assignment3.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# 1. Why are functions advantageous to have in your programs?
+# ANS: Function has following advantages:
+#     1) It divides code into modules
+#     2) It improves readability
+#     3) It allows reusability
+# ********************************************************************************************************************************
+# 2. When does the code in a function run: when it's specified or when it's called?
+# 
+# ANS: The code in function will run when the function is called
+# 
+# ********************************************************************************************************************************
+# 
+# 3. What statement creates a function?
+# 
+# ANS: The 'def' keyword following the 'function name' will create a function. 
+#      For example:
+#          def new_function():
+#             print("Hello World")
+#         This will create a function named 'new_function'
+#         *******************************************************************************************************************************
+#         
+# 4. What is the difference between a function and a function call?
+# 
+# ANS: Function contains the part of code that performs some operations to give certain output. 
+#      Whereas, Function call will ask to execute the code of function at given point of time.Unless a function is called there is no use of that function.
+#      ********************************************************************************************************************************
+#      
+# 5. How many global scopes are there in a Python program? How many local scopes?
+# 
+# ANS: There is one global scope in python program. Local scope is created as and when function is being called.
+# 
+# ********************************************************************************************************************************
+# 
+# 6. What happens to variables in a local scope when the function call returns?
+# 
+# ANS: When the function call returns, the local scope variables are forgotten and thus the local scope is destroyed.
+# 
+# ********************************************************************************************************************************
+# 
+# 7. What is the concept of a return value? Is it possible to have a return value in an expression?
+# 
+# ANS: The return value is the value sent by function to the function caller. Yes, it is possible to have a return valu in an expression.
+# 
+# ********************************************************************************************************************************
+# 
+# 8. If a function does not have a return statement, what is the return value of a call to that function?
+# 
+# ANS: When there is no return statement, the return value of a call to that function is None.
+# ********************************************************************************************************************************
+# 
+# 
+# 9. How do you make a function variable refer to the global variable?
+# 
+# ANS: We can make a function variable refer to the global variable by using global statements.
+# 
+# ********************************************************************************************************************************
+# 
+# 10. What is the data type of None?
+# 
+# ANS: Data typr of None is NoneType
+# 
+# ********************************************************************************************************************************
+# 
+# 11. What does the sentence import areallyourpetsnamederic do?
+# 
+# ANS: The sentence import areallyourpetsnamederic imports the module nameareallyourpetsnamederic.
+# 
+# ********************************************************************************************************************************
+# 
+# 12. If you had a bacon() feature in a spam module, what would you call it after importing spam?
+# 
+# ANS: it can be called as spam.bacon()
+# 
+# ********************************************************************************************************************************
+# 
+# 13. What can you do to save a programme from crashing if it encounters an error?
+# 
+# ANS: Use try clause
+# 
+# ********************************************************************************************************************************
+# 
+# 14. What is the purpose of the try clause? What is the purpose of the except clause?
+# 
+# ANS: Try clause is used for the statements/code that can cause error
+#     and except clause is used to give message if error occurs
+#     
+# ********************************************************************************************************************************
+
+# In[ ]:
+
+
+
+
